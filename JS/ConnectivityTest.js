d09118a9b6be876f03ed8df86d4f8f059f232a24
K <104524788+Keywos@users.noreply.github.com>
2023-02-19 23:05:33 +0800

    2

--- JS/ConnectivityTest.js
+++ JS/ConnectivityTest.js
@@ -1,3 +1,4 @@
+/* 加入了百度和bili测速,感谢@yibeizipeini原脚本来自于https://raw.githubusercontent.com/yibeizipeini/JavaScript/Surge/ConnectivityTest.js*/
 const REQUEST_HEADERS = {
   'User-Agent':
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36',
