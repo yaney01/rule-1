9871ccecfbef6f53c495289f8ab8c9371b81991d
K <104524788+Keywos@users.noreply.github.com>
2023-03-17 03:03:33 +0800

    2

--- JS/tl.js
+++ JS/tl.js
@@ -9,20 +9,12 @@ hostname = %APPEND% m.pvp.xoyo.com
 mm = type=http-response,pattern=^https\:\/\/m\.pvp\.xoyo\.com\/conf\/server-mapping,requires-body=1,max-size=0,script-path=https://github.com/Keywos/rule/raw/main/JS/tl.js
 */
 let obj = JSON.parse($response.body);
-obj.data.splashConfNew.forEach(item => {
-  
-	item.aliveTime = 0;
-	item.status = item.status.replace("ON", "OFF");
-	/*
-	item.urlX = "";
-	item.url = "";
-	item.title = "去你的广告";
-	*/
-	let props = ["urlX", "url", "title"];
+obj.data.splashConfNew.forEach(item => {  
+  item.aliveTime = 0;
+  item.status = item.status.replace("ON", "OFF");
+  let props = ["urlX", "url", "title"];
   if (obj.data) {
   props.forEach((prop) => {
     delete item[prop];
-  });
-}
-});
+  });}});
 $done({body: JSON.stringify(obj)});
