eea99e1d8ffe68ae8ebf7bfb29c6d3a2d731fb3f
K <104524788+Keywos@users.noreply.github.com>
2023-04-06 02:35:50 +0800

    Update ua.js
    Signed-off-by: K <104524788+Keywos@users.noreply.github.com>

--- JS/ua.js
+++ JS/ua.js
@@ -1,7 +1,23 @@
-let url = $request.url,
-    body = null;
-    if (url.includes("amdc/mobileDispatch")) {
-    let e = $request.headers,
-        t = e["User-Agent"] || e["user-agent"];
-    t.includes("AMap") || t.includes("Cainiao") ? $done() : $done({})
-}$done({});
+// let url = $request.url;
+// if (url.includes("amdc/mobileDispatch")) {
+// let headers = $request.headers,
+// userAgent = headers["User-Agent"] || headers["user-agent"];
+// $done(userAgent.includes("AMap") || userAgent.includes("Cainiao") ? undefined : {});
+// }
+
+// const url = $request.url;
+// const header = $request.headers;
+// let ua = header["User-Agent"] || header["user-agent"];
+// if (url.includes("/amdc/mobileDispatch")) {
+//   if (ua.includes("AMapiPhone") || ua.includes("Cainiao")) {
+//     $done({ status: "HTTP/1.1 404 Not Found" });
+//     return;}}
+// $done({});
+
+const url = $request.url;
+const header = $request.headers;
+const ua = header["User-Agent"] || header["user-agent"];
+if (url.includes("/amdc/mobileDispatch") && (ua.includes("AMapiPhone") || ua.includes("Cainiao"))) {
+$done({ status: "HTTP/1.1 404 Not Found" });
+return;}
+$done({});
