3694f5e5687d0313f982d33012d822e271a2a846
K <104524788+Keywos@users.noreply.github.com>
2023-04-09 02:55:06 +0800

    Update UA2.js
    Signed-off-by: K <104524788+Keywos@users.noreply.github.com>

--- JS/UA2.js
+++ JS/UA2.js
@@ -17,6 +17,23 @@ hostname = %APPEND% *.bing.com, bing.com
 
 [Script]
 bing-edge-ua = type=http-request,pattern=^http(s)?:\/\/(?:.*\.)?bing\.com,script-path=useragent.js,requires-body=false
+
+// let url = $request.url;
+// if (url.includes("amdc/mobileDispatch")) {
+// let headers = $request.headers,
+// userAgent = headers["User-Agent"] || headers["user-agent"];
+// $done(userAgent.includes("AMap") || userAgent.includes("Cainiao") ? undefined : {});
+// }
+
+// const url = $request.url;
+// const header = $request.headers;
+// let ua = header["User-Agent"] || header["user-agent"];
+// if (url.includes("/amdc/mobileDispatch")) {
+//   if (ua.includes("AMapiPhone") || ua.includes("Cainiao")) {
+//     $done({ status: "HTTP/1.1 404 Not Found" });
+//     return;}}
+// $done({});
+
 */
 const headers = $request.headers;
 
