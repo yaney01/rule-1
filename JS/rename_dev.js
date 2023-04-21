5013bd29914481703206d4dcbf7c9b67741eb0a7
K <104524788+Keywos@users.noreply.github.com>
2023-04-21 15:56:40 +0800

    Update rename_dev.js
    Signed-off-by: K <104524788+Keywos@users.noreply.github.com>

--- JS/rename_dev.js
+++ JS/rename_dev.js
@@ -118,39 +118,42 @@ function simplify(cc) {
 
 // 主函数
 function operator(proxies) {
-
-// 在这里打印一下传入的正则表达式
-console.log("正则表达式："+ $arguments['regex'] );
-
-  proxies.map((res) => {
-    const resultArray = [airport];
-    var matched = false
-    for (const elem of Object.keys(countries)) {
-      if (simplify(res.name).indexOf(elem) !== -1) {
-        countries[elem][1] += 1;
-        if (!autofill) {
-          resultArray.push(countries[elem][0], countries[elem][1].toString().padStart(2, '0'));
-        } else {
-          resultArray.push(countries[elem][0], countries[elem][1].toString().padStart(autofill, '0'));
-        }  
-        matched = true
-        break;
+    const toBeDeleted = [];
+    proxies.forEach((res) => {
+      const resultArray = [airport];
+      var matched = false;
+      for (const elem of Object.keys(countries)) {
+        if (simplify(res.name).indexOf(elem) !== -1) {
+          countries[elem][1] += 1;
+          if (!autofill) {
+            resultArray.push(countries[elem][0], countries[elem][1].toString().padStart(2, '0'));
+          } else {
+            resultArray.push(countries[elem][0], countries[elem][1].toString().padStart(autofill, '0'));
+          }
+          matched = true;
+          break;
+        };
       };
-    };
-    if (!matched) {
-      resultArray.push(res.name);
-    };
-    Object.keys(others).forEach((elem, index) => {
-      if (simplify(res.name).indexOf(elem) !== -1) {
-        resultArray.splice(2, 0, others[elem]);
-     
+      if (!matched) {
+        resultArray.push(res.name);
+        toBeDeleted.push(res);
+      };
+      Object.keys(others).forEach((elem, index) => {
+        if (simplify(res.name).indexOf(elem) !== -1) {
+          resultArray.splice(2, 0, others[elem]);
+        }
+      });
+      res.name = resultArray.join(' ');
+    });
+    // Remove
+    toBeDeleted.forEach((proxy) => {
+      const index = proxies.indexOf(proxy);
+      if (index !== -1) {
+        proxies.splice(index, 1);
       }
     });
-    res.name = resultArray.join(' ');
-  });
-  if ($arguments.clear) {
-    proxies = stripOnes(proxies);
-    proxies = proxies.filter(item => !nameclear.test(item.name));
-  };
+    if ($arguments.del1) {
+      proxies = stripOnes(proxies);
+    };
     return proxies;
-}
+  }
