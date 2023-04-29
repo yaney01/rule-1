a09fb54db6433b48b9e33229d6896a671bba5625
K <104524788+Keywos@users.noreply.github.com>
2023-04-29 14:49:31 +0800

    ua

--- JS/cnua.js
+++
@@ -1,7 +0,0 @@
-let url = $request.url,
-    body = null;
-    if (url.includes("amdc/mobileDispatch")) {
-    let e = $request.headers,
-        t = e["User-Agent"] || e["user-agent"];
-    t.includes("AMap") || t.includes("Cainiao") ? $done() : $done({})
-}$done({});
--- JS/ua.js
+++ JS/ua.js
@@ -1,7 +1,6 @@
-const url = $request.url;
-const header = $request.headers;
-const ua = header["User-Agent"] || header["user-agent"];
-if (url.includes("/amdc/mobileDispatch") && (ua.includes("AMapiPhone") || ua.includes("Cainiao"))) {
-$done({ status: "HTTP/1.1 404 Not Found" });
-return;}
-$done({});
+let header = $request.headers
+    let ua = header["User-Agent"] || header["user-agent"]
+    if (ua.includes("AMap") || ua.includes("Cainiao")) {
+        if ('undefined' !== typeof $task) $done({status: 'HTTP/1.1 404 Not Found'})
+        else $done()
+    } else $done({})
