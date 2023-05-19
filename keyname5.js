5a17345caa7d5989961240303d2df21bb01054b0
K <704496037@qq.com>
2023-05-19 15:12:31 +0800

    xx

--- keyname5.js
+++ keyname5.js
@@ -210,7 +210,7 @@ async function operator(proxies) {
    
           const outip = await IPAPI(proxy);
         //inip.regionName +""+
-          proxy.name = inip.city.replace("市", "")+FGF+ass+FGF+ outip.country;
+          proxy.name = inip.city.replace(/特别市|市/g, "")+FGF+ass+FGF+ outip.country;
         //   // 去重 入口/落地IP
           proxy.qc = inip.query + "|" + outip.query;
         } catch (err) {}
