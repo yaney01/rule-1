b7ff145df01985dadf666b58eee0f7378db099f7
K <704496037@qq.com>
2023-05-24 15:00:31 +0800

    xx

--- loon/subs.js
+++
@@ -1,9 +0,0 @@
-let appName = $persistentStore.read("appName")
-let author = $persistentStore.read("author")
-let appType = $persistentStore.read("appType")
-let price = $persistentStore.read("price")
-
-let test = $persistentStore.read("测试")
-
-console.log(appName + " " + author +  " " + appType +  " " + price)
-$done()
