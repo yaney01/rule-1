da418785b589e30b44a947c6bda081527463ed83
K <104524788+Keywos@users.noreply.github.com>
2023-05-27 01:50:10 +0800

    dd

---
+++ test/bug.js
@@ -0,0 +1,4 @@
+//const subs = JSON.parse($request["body"]);
+//const body = $request["body"];let subs;if (body) {subs = JSON.parse(body);} else {subs = {};};const subname =subs.name;
+// const idname =subs.process[1].id;
+// console.log("\nname: "+subname+"\nid: "+idname)
