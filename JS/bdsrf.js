3723b0f5d4d0e145d99b85cbfd3d96a82e79949f
K <704496037@qq.com>
2023-05-29 21:55:43 +0800

    cc

--- JS/bdsrf.js
+++ JS/bdsrf.js
@@ -1,30 +1,18 @@
 const okk = JSON.parse($response.body);
 if (/imrobot\/v1\/pub\/relation\/get_robot_list/.test($request.url)) {
   okk.data.robot_info.forEach((robot) => {
-robot.friend_bg_img = `https://raw.githubusercontent.com/Keywos/rule/main/mocks/bd/robot${Math.floor(Math.random() * 12) + 1}.jpg`;
-  });
+robot.friend_bg_img = `https://raw.githubusercontent.com/Keywos/rule/main/mocks/bd/robot${Math.floor(Math.random() * 12) + 1}.jpg`;});
 } else if (/v5\/custom_page\/layout/.test($request.url)) {
-  const keyi=["功能入口","表情图标iOSan","一丢儿颜文字标题栏-iOSan","活动中心标题栏iOS","字体banner",];
-  const keys = ["字体banner", "活动中心标题栏iOS"];
+  const keyi=["\u529f\u80fd\u5165\u53e3","\u8868\u60c5\u56fe\u6807iOSan","\u4e00\u4e22\u513f\u989c\u6587\u5b57\u6807\u9898\u680f-iOSan","\u6d3b\u52a8\u4e2d\u5fc3\u6807\u9898\u680fiOS","\u5b57\u4f53banner",];
+  const keys = ["\u5b57\u4f53banner", "\u6d3b\u52a8\u4e2d\u5fc3\u6807\u9898\u680fiOS"];
   if (okk.data.page_layout_content && Array.isArray(okk.data.page_layout_content)) {
     okk.data.page_layout_content = okk.data.page_layout_content.filter(
-      ({ module_desc }) => keyi.includes(module_desc)
-    );}
+      ({ module_desc }) => keyi.includes(module_desc));}
   if (okk.data.page_data_content && Array.isArray(okk.data.page_data_content)) {
-    okk.data.page_data_content = okk.data.page_data_content.filter(
-      ({ module_desc }) => keyi.includes(module_desc)
-    );
-    okk.data.page_data_content = okk.data.page_data_content.filter(
-      (module) => !keys.some((keyword) => module.module_desc.includes(keyword))
-    );
-  }
+      okk.data.page_data_content = okk.data.page_data_content.filter(
+    ({ module_desc }) => keyi.includes(module_desc) && !keys.some((keyword) => module_desc.includes(keyword)));}
 } else if (/v5\/custom_page\/getdata/.test($request.url)) {
   if (okk.data && Array.isArray(okk.data)) {
-    okk.data.forEach((item) => {
-      if (item.items) {
-        item.items = [];
-      }
-    });
-  }
+    okk.data.forEach((item) => {if (item.items) {item.items = [];}});}
 }
 $done({ body: JSON.stringify(okk) });
