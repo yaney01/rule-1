3a74c553d6820f91a51c8bb7b069b68023e8b180
K <704496037@qq.com>
2023-05-31 23:26:31 +0800

    66

--- JS/cainiao.js
+++ JS/cainiao.js
@@ -3,6 +3,7 @@ switch (true) {
   case /cainiao\.nbpresentation\.protocol\.homepage\.get\.cn/.test($request.url):
     //首页 红点，闪烁图标，推广,ljjq 领寄件券,ttlhb 天天领红包,gjjf 裹酱积分,bgxq 包裹星球,cngreen 绿色家园,tcqs 同城取送,qydq 亲友代取
     const kkkey = ["gjjf", "bgxq", "cngreen", "ttlhb", "ljjq"];
+    if(okk.data.result.dataList){
     okk.data.result.dataList = okk.data.result.dataList.filter((item) => {
       if (item.type == "big_banner_area_v870") {
         return false; // "big_banner_area_v870" 新人福利
@@ -19,7 +20,7 @@ switch (true) {
         item.rightIcon = null;
         item.bubbleText = null;
       });
-    });
+    });}
     break;
   case /cainiao\.guoguo\.nbnetflow\.ads\.mshow/.test($request.url):
     const item = ["1316", "1332","1275","1308","1340"];
