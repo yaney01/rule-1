61720991825fc490451f3bc904575a0519bb20e2
K <704496037@qq.com>
2023-06-02 21:38:59 +0800

    cn

--- JS/cainiao.js
+++ JS/cainiao.js
@@ -1,42 +1,105 @@
 let okk = JSON.parse($response.body);
 switch (true) {
-  case /cainiao\.nbpresentation\.protocol\.homepage\.get\.cn/.test($request.url):
-    //首页 红点，闪烁图标，推广,ljjq 领寄件券,ttlhb 天天领红包,gjjf 裹酱积分,bgxq 包裹星球,cngreen 绿色家园,tcqs 同城取送,qydq 亲友代取
-    const kkkey = ["gjjf", "bgxq", "cngreen", "ttlhb", "ljjq"];
-    if(okk.data.result.dataList){
-    okk.data.result.dataList = okk.data.result.dataList.filter((item) => {
-      if (item.type == "big_banner_area_v870") {
-        return false; // "big_banner_area_v870" 新人福利
-      }
-      if (item.bizData.items) {
-        item.bizData.items = item.bizData.items.filter((subItem) => {
-          return !kkkey.includes(subItem.key); // 过滤掉 key 在 kkkey 中的子元素
+  case /cainiao\.nbpresentation\.protocol\.homepage\.get\.cn/.test(
+    $request.url):
+    if (okk.data.result) {
+      let res = okk.data.result;
+      if (res.dataList) {
+        res.dataList = res.dataList.filter((i) => {
+          // 顶部图标
+          if (i.type.includes("icons_scroll_unable")) {
+            if (i.bizData.items) {
+              const item = [
+                "bgxq", // 包裹星球
+                "cngy", // 免费领水果
+                "cngreen", // 绿色家园
+                "gjjf", // 裹酱积分
+                "ljjq", // 领寄件券
+                "ttlhb", // 天天领红包
+              ];
+              i.bizData.items = i.bizData.items.filter(
+                (ii) => !item.includes(ii.key)
+              );
+            }
+          } else if (i.type.includes("big_banner_area")) {
+            // 新人福利
+            return false;
+          } else if (i.type.includes("promotion")) {
+            // 促销活动
+            return false;
+          } else {
+            return true;
+          }
+          res.dataList.forEach((i) => {
+            i.bizData.items.forEach((ii) => {
+              ii.rightIcon = null;
+              ii.bubbleText = null;
+            });
+          });
         });
       }
-      return true; //其他保留
-    });
-    okk.data.result.dataList.forEach((data) => {
-      data.bizData.items.forEach((item) => {
-        item.rightIcon = null;
-        item.bubbleText = null;
-      });
-    });}
+    }
     break;
   case /cainiao\.guoguo\.nbnetflow\.ads\.mshow/.test($request.url):
-    const item = ["1316", "1332","1275","1308","1340"];
+    const item = ["1316", "1332", "1275", "1308", "1340"];
     for (let i of item) {
       if (okk.data?.[i]) {
         delete okk.data[i];
       }
     }
     break;
+  case /nbpresentation\.homepage\.merge\.get\.cn/.test($request.url):
+    if (okk.data) {
+      // 移除 反馈组件
+      const item = [
+        "mtop.cainiao.nbmensa.research.researchservice.acquire.cn@0",
+        "mtop.cainiao.nbmensa.research.researchservice.acquire.cn@1",
+        "mtop.cainiao.nbmensa.research.researchservice.acquire.cn@2",
+        "mtop.cainiao.nbmensa.research.researchservice.acquire.cn@3",
+      ];
+      for (let i of item) {
+        if (okk.data?.[i]) {
+          delete okk.data[i];
+        }
+      }
+    }
+    break;
+  case /nbpresentation\.pickup\.empty\.page\.get\.cn/.test($request.url):
+    if (okk.data.result) {
+      let ggContent = okk.data.result.content;
+      if (ggContent.middle) {
+        ggContent.middle = ggContent.middle.filter(
+          (i) =>
+            ![
+              "guoguo_pickup_empty_page_relation_add", // 添加亲友
+              "guoguo_pickup_helper_feedback", // 反馈组件
+              "guoguo_pickup_helper_tip_view", // 取件小助手
+            ].includes(i.template.name)
+        );
+      }
+    }
+    break;
   case /guoguo\.nbnetflow\.ads\.show\.cn/.test($request.url):
-    // 底部标签页
-  if (okk.data.result) {
-    okk.data.result = okk.data.result.filter(
-    (i) =>!(i?.materialContentMapper?.group_id?.includes("entertainment") ||
-        (i?.materialContentMapper?.bgImg &&
-          i?.materialContentMapper?.advRecGmtModifiedTime)));}
+    if (okk.data.result) {
+      okk.data.result = okk.data.result.filter((i) => {
+        const stad = i?.materialContentMapper?.templateCode;
+        const stadcn = i?.id;
+
+        const group_id = i?.materialContentMapper?.group_id;
+        const bgImg = i?.materialContentMapper?.bgImg;
+        const adTime = i?.materialContentMapper?.advRecGmtModifiedTime;
+
+        const stakey = "fly_splash_picture"; // 开屏
+        const hebing = new Set(["entertainment", "kuaishou_banner"]); // 底部标签页活动 、快手banner
+
+        return !(
+          stadcn?.includes("29524") ||
+          stad?.includes(stakey) ||
+          (group_id && hebing.has(group_id)) ||
+          (bgImg && adTime)
+        );
+      });
+    }
     break;
   default:
     break;
