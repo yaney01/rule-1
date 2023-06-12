81c8414a3450b7843dc5103d491630fa3b825ecd
K <704496037@qq.com>
2023-06-12 15:04:09 +0800

    6

--- JS/surgeithome.js
+++ JS/surgeithome.js
@@ -1,11 +1,7 @@
 let i = JSON.parse($response.body);
-i.data.list = i.data.list.filter(function(item) {
-return item.feedType !== 10002;  //轮播
-});
-i.data.list = i.data.list.filter(function(item) {
-return item.feedType !== 10003;  //置顶
-});
 i.data.list = i.data.list.filter(item => {
-  return item.feedType !== 10000 || (item.feedContent.smallTags[0]?.text === null || !item.feedContent.smallTags[0].text.includes("广告"));
+  return item.feedType !== 10002 
+  && item.feedType !== 10003 
+  && (item.feedType !== 10000 || (item.feedContent.smallTags[0]?.text === null || !item.feedContent.smallTags[0].text.includes("广告")));
 });
 $done({body: JSON.stringify(i)});
