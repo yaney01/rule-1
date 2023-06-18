7c2207ae2bc5d47136fe1a31f29d35dba5afa138
K <704496037@qq.com>
2023-06-18 16:05:27 +0800

    6

--- JS/loonpushBarkapps.js
+++ JS/loonpushBarkapps.js
@@ -1,40 +1,47 @@
-//@key
+/*@key 小白脸
+#!desc= 利用Barkapp推送限免app，需要自己下载Bark并找到key填进argument
+# 持久化缓存每次获取的列表，每个app只会推送一次，持久化数据位置: uuidkeys
+Surge
+[MITM]
+hostname = %APPEND% barkapp.key.com
+[Script]
+BarkappREQ = type=http-request,pattern=^https?:\/\/barkapp\.key\.com,script-path=https://raw.githubusercontent.com/Keywos/rule/main/JS/surgepushBarkapps.js,requires-body=true
+*/
+// Barkapps=type=cron,cronexp=0 */6 * * *,wake-system=1,timeout=120,script-path=https://raw.githubusercontent.com/Keywos/rule/main/JS/surgepushBarkapps.js,argument=
 let key = this.$argument ?? "";
 const loonkey = $persistentStore.read("Bark推送KEY");
 if (loonkey !== undefined) {
   key = loonkey;
 }
-function getTI(e) {
-  const k = new Date(e * 1000);
-  const y = k.toLocaleString("en-CN");
-  return y.replace(/:\d\d$|,|^\d\d/g, "");
-}
-
-function sK(s, e) {
-  return s.split("\n", e).join(" ");
-}
-
+let url;if (typeof $request !== 'undefined' && $request.url)
+{url = $request.url;} else {url = '0';}
 Promise.all([
   (async () => {
     try {
     if(key !==""){
-      const uuk = await tKey({
-          url: "https://api.gofans.cn/v1/m/app_records?page=1&limit=10",
+      if (/barkapp\.key\.com/.test(url)) {
+            let spurl = url.split("&")[1];
+          const uuapp = await tKey(
+            {url: `https://api.gofans.cn/v1/m/apps/${spurl}`,
+              headers: {
+                referer: "https://m.gofans.cn/",
+                origin: "https://m.gofans.cn",
+              },},500,"get");
+          $done({response: {status: 302,headers: {Location: `${uuapp.track_url}`}}}); 
+      } else {
+      const uuk = await tKey({url: "https://api.gofans.cn/v1/m/app_records?page=1&limit=10",
           headers: {
             referer: "https://m.gofans.cn/",
             origin: "https://m.gofans.cn",
-          },},500,"get"
-      );
-      let reu = $persistentStore.read("uuidkey");
+          },},500,"get");
+      let reu = $persistentStore.read("uuidkeys");
       let readuuid = reu ? JSON.parse(reu) : [""];
       if (readuuid.length > 10) {
         readuuid.splice(0, readuuid.length - 10);
       }
-
       let uuidList = uuk.data.filter(function (item) {
         return !readuuid.includes(item.uuid);
       });
-
       let uuids = uuidList
         .filter(function (i) {
           return i.uuid !== undefined;
@@ -47,58 +54,40 @@ Promise.all([
       if (uuids && uuids.length) {
         readuuid.push(...uuids);
         let writeuuid = JSON.stringify(readuuid);
-        $persistentStore.write(writeuuid, "uuidkey");
+        $persistentStore.write(writeuuid, "uuidkeys");
       }
-
       await Promise.all(
         uuidList.map(async (ik, nc) => {
-          let uuid = ik.uuid;
-          // let {primary_genre_name,updated_at,original_price,price,name,uuid} = ik
-          const uuapp = await tKey(
-            {url: `https://api.gofans.cn/v1/m/apps/${uuid}`,
-              headers: {
-                referer: "https://m.gofans.cn/",
-                origin: "https://m.gofans.cn",
-              },},500,"get"
-          );
-
-          let {icon,name: napp,track_url,description,kind_name,original_price: yj,price: xj,updated_at,
-          } = uuapp;
-          // console.log(icon + track_url + kind_name);
-          const pushapp = await tKey(
-            {
-              url: "https://api.day.app/push",
-              headers: { "Content-Type": "application/json; charset=utf-8" },
-              body: JSON.stringify({
-                title: kind_name + ": " + napp,
-                body: `${getTI(updated_at)}   CNY: ${yj} ➟ ${xj}\n${sK(
-                  description,
-                  1
-                )}`,
-                //版本:${version}
-                device_key: key,
-                icon: icon,
-                //badge: "921", //通知数量
-                level: "active",
-                //active：默认值，系统会立即亮屏显示通知
-                //timeSensitive：时效性通知，可在专注状态下显示通知。
-                //passive：仅将通知添加到通知列表，不会亮屏提醒。
-                group: kind_name, // ios or mac分组
-                url: track_url,
-              }),
-            },
-            500,
-            "post"
-          );
-        })
-      );
-    }
-      $done();
+            let {icon,name: napp,uuid,description,original_price: yj,price: xj,updated_at} = ik;
+            console.log(icon)
+            const pushapp = await tKey(
+              {
+                url: "https://api.day.app/push",
+                headers: { "Content-Type": "application/json; charset=utf-8" },
+                body: JSON.stringify({
+                  title: napp,
+                  body: `${getTI(updated_at)}   CNY: ${yj} ➟ ${xj}\n${sK(description,1)}`,
+                  device_key: key,
+                  icon: icon,
+                  //badge: "921", //通知数量
+                  level: "passive",
+                  //active：默认值，系统会立即亮屏显示通知
+                  //timeSensitive：时效性通知，可在专注状态下显示通知。
+                  //passive：仅将通知添加到通知列表，不会亮屏提醒。
+                  url:`https://barkapp.key.com/key&${uuid}`
+                }),
+              },500,"post");
+            }));}
+      } else {
+        $done($notification.post("", "", "未填写key"));
+      }
+      
     } catch (error) {
       $done($notification.post("", "", "错误,反馈@key"));
     }
   })(),
 ]);
+
 function sM(s, e) {
   if (s.length > e) {
     return s.slice(0, e);
@@ -108,7 +97,15 @@ function sM(s, e) {
     return s;
   }
 }
-
+function getTI(e) {
+    const k = new Date(e * 1000);
+    const y = k.toLocaleString("en-CN");
+    return y.replace(/:\d\d$|,|^\d\d/g, "");
+  }
+  
+function sK(s, e) {
+return s.split("\n", e).join(" ");
+}
 async function tKey(options, timeout, method = "get") {
   let rec = 3,
     cskey = 1;
@@ -140,7 +137,6 @@ async function tKey(options, timeout, method = "get") {
                       case type.includes("text/plain"):
                         // console.log("text/plain");
                         break;
-
                       case type.includes("image/svg+xml"):
                         // console.log("image/svg+xml");
                         let lines = data.split("\n");
@@ -157,7 +153,6 @@ async function tKey(options, timeout, method = "get") {
                         break;
                     }
                     break;
-
                   case 204:
                     // console.log("204");
                     // console.log(endtime);
