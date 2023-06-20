12a50b4c62b38a20a4ce3de924769d149dcca6ff
K <704496037@qq.com>
2023-06-20 19:54:59 +0800

    6

--- loon/7.js
+++ loon/7.js
@@ -359,8 +359,14 @@ function nfTest() {
             node: nodeName,
             timeout: 6000, //ms
             headers: {
-                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 Safari/605.1.15',
-            }
+                              // 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36',
+                'referer': 'https://www.netflix.com/',
+                'origin': 'https://www.netflix.com',
+                // 'sec-ch-ua': 'Chromium";v="116", "Not)A;Brand";v="24", "Microsoft Edge";v="116',
+                // 'sec-ch-ua-mobile': '?0',
+                // 'sec-ch-ua-platform': 'macOS',
+                        }
         }
         
         $httpClient.get(params, (errormsg,response,data) => {
