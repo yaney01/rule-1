2ff3708b392066259fc05d4a72f93bb9375abdee
K <104524788+Keywos@users.noreply.github.com>
2023-06-29 18:01:07 +0800

    6

--- JS/tk.js
+++ JS/tk.js
@@ -1,7 +1,6 @@
 let keyus={日本: "JP", 韩国: "KR", 英国:"UK", 美国:"US", 台湾:"TW", 香港:"HK", 新加坡:"SG", 法国:"FR"},
   lk = $persistentStore.read("TikTok解锁地区"),
-  loc = keyus[lk] || "KR",
-  url = $request.url;
+  loc = keyus[lk] || "KR", url = $request.url;
 if (/\?residence=\$|mcc_mnc=/.test(url)) {
     console.log(url)
     url = url.replace(/_region=CN\&/g,`_region=${loc}&`).replace(/mcc_mnc=4/g,"mcc_mnc=2").
