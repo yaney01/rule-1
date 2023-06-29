080d02a0dffd5bb94347c2a57b8043bd6ee9603e
K <104524788+Keywos@users.noreply.github.com>
2023-06-29 18:39:37 +0800

    6

--- JS/tk.js
+++ JS/tk.js
@@ -3,7 +3,7 @@ let keyus={日本: "JP", 韩国: "KR", 英国:"UK", 美国:"US", 台湾:"TW", �
   loc = keyus[lk] || "KR", url = $request.url;
 if (/\?residence=\$|mcc_mnc=/.test(url)) {
     console.log(url)
-    url = url.replace(/_region=CN\&/g,`_region=${loc}&`).replace(/mcc_mnc=4/g,"mcc_mnc=2").
+    url = url.replace(/_region=CN\&/g,`_region=${loc}&`).replace(/mcc_mnc=4/g,"mcc_mnc=2")
     console.log(url)
   $done({response: {status: 307, headers: {Location: url}}});
 } else {
