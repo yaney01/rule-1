2620d5f56f06778d7ee570eb587012789799e5ba
K <704496037@qq.com>
2023-06-30 00:05:49 +0800

    6

--- loon/tk.js
+++ loon/tk.js
@@ -1,5 +1,9 @@
-let keyus={日本: "JP", 韩国: "KR", 英国:"UK", 美国:"US", 台湾:"TW", 香港:"HK", 新加坡:"SG", 法国:"FR", 马来西亚:"MY", 菲律宾:"PH", 泰国:"TH"},
+let keyus={日本: "JP", 韩国: "KR", 英国:"UK", 美国:"US", 台湾:"TW", 香港:"HK", 新加坡:"SG", 法国:"FR", 马来西亚:"MY", 菲律宾:"PH", 泰国:"TH", 参数传入:"inkey"},
 lk = $persistentStore.read("TikTok解锁地区"),loc = keyus[lk] || "KR",url = $request.url;
+if(loc == "inkey"){
+  inkeys = $persistentStore.read("手动输入地区代码");
+  loc = inkeys
+}
 if (/tnc|dm/.test(url)) {
   url = url.replace(/\/\?.+/g,"\?");
   const response = {
