989f39d712a916dc1570080bfbf1421b9b4e9e8e
K <704496037@qq.com>
2023-06-30 15:09:11 +0800

    6

--- loon/QX_to_Loon_Auto.js
+++ loon/QX_to_Loon_Auto.js
@@ -7,6 +7,7 @@
 感谢@mieqq 提供的replace-body.js
 插件图标用的 @Keikinn 的 StickerOnScreen项目 以及 @Toperlock 的图标库项目，感谢，感谢
 **/
+
 let yreq = $request, yurl = yreq.url, loonua = false, loonurl = false,
 	yua = yreq.headers["User-Agent"] || yreq.headers["user-agent"];
 (yua.includes("Loon")) && (loonua = true);
