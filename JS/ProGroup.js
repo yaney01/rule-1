681923063d3bcd0479c5a774e7da9601ee25f61c
K <704496037@qq.com>
2023-07-06 01:20:07 +0800

    GroupAuto

--- JS/ProGroup.js
+++ JS/ProGroup.js
@@ -1,13 +1,11 @@
 /*
 作用: 根据 Surge httpApi 请求 (最近请求详情:速度、 参数指定的策略组:与其对应HASH节点延时、节点名)
 策略: 根据 api 返回的节点 (速度:持久化缓存非线性权重) 与 (延时:持久化缓存) 对节点进行优选, 不用担心费流量，因为通过httpApi取的最近请求里的速度数据
-
 参数: 
 group=          你的策略组名
 tolerance=10    容差10ms 小于10ms则不切换节点
 timecache=18    缓存到期时间(小时) 或 超过40个数据会清理旧的数据
 push            加参数为开启通知, 不加参数则不通知
-
 #!name=GroupAuto
 #!desc=根据 api 返回的节点 (速度:持久化缓存非线性权重) 与 (延时:持久化缓存) 对节点进行优选
 [Panel]
@@ -17,7 +15,6 @@ GroupAuto = script-name=GroupAuto,update-interval=3
 GroupAuto = type=generic,timeout=3,script-path=https://github.com/Keywos/rule/raw/main/JS/ProGroup.js,argument=group=VPS&tolerance=10&timecache=18
 # 定时自动运行 5分钟一次
 Cron_GroupAuto = type=cron, cronexp= "0/5 * * * *", timeout=5,script-path=https://github.com/Keywos/rule/raw/main/JS/ProGroup.js,argument=group=VPS&tolerance=1&timecache=18
-
 */
 let Groupkey = "VPS",tol = "10",th = "18",p = "", j = "", push = false;
 if (typeof $argument !== "undefined" && $argument !== "") {
@@ -26,9 +23,8 @@ if (typeof $argument !== "undefined" && $argument !== "") {
   th = ins.timecache ? ins.timecache : th;
   tol = ins.tolerance ? ins.tolerance : tol;
   push = ins.push ? ins.push : false;
-  // console.log(ins);
+  
 }
-
 (async () => {
   // 获取请求记录详情过滤获取最高上下传速度
   const Speed = await httpAPI("/v1/requests/recent");
@@ -46,7 +42,6 @@ if (typeof $argument !== "undefined" && $argument !== "") {
       avgts[name][1] += 1;
     }
   });
-
   // 根据提取的速度调整优先级
   const ioSpee = Object.entries(avgts).reduce(
     (obj, [name, [sum, count]]) => {
@@ -56,21 +51,14 @@ if (typeof $argument !== "undefined" && $argument !== "") {
     },
     {}
   );
-  // console.log("上下载最大速度");
-  // console.log(ioSpee);
-
   // 获取策略组内节点
   const proxy = await httpAPI("/v1/policy_groups");
   // 提取 name 属性的值到一个新数组
   const proxyName = proxy[Groupkey].map((obj) => obj.name);
-  // console.log("所有策略");
-  // console.log(JSON.stringify(proxyName));
 
   // 请求测速选择的策略组
   const testReq = await httpAPI("/v1/policy_groups/test", "POST", (body = { group_name: Groupkey }));
   const nowproxy = testReq.available[0];
-  // console.log("当前策略");
-  // console.log(nowproxy);
 
   // 获取所有延时测速结果
   const testGroup = await httpAPI("/v1/policies/benchmark_results");
@@ -85,21 +73,18 @@ if (typeof $argument !== "undefined" && $argument !== "") {
   });
   //.filter(i => i.ms !== -1); //过滤测速失败的
   //console.log(JSON.stringify(resultsMs, null, 2));
-
   // 提取的测速结果
   const kv = resultsMs.reduce((obj, { name, ms }) => {
     obj[name] = ms;
     return obj;
   }, {});
-  // console.log(kv);
-
+  
   // 速度高的优选权重变高
   for (let key in ioSpee) {
     if (kv.hasOwnProperty(key)) {
       kv[key] = reSpeed(Number(ioSpee[key]), Number(kv[key]));
     }
   }
-
   const t = new Date().getTime();
   // 读取存储的数据
   const readData = $persistentStore.read("KEY_GroupAuto");
@@ -125,7 +110,6 @@ if (typeof $argument !== "undefined" && $argument !== "") {
   k[Groupkey] = GT;
   $persistentStore.write(JSON.stringify(k), "KEY_GroupAuto");
   //console.log(JSON.stringify(k[Groupkey],null,2));
-
   const AllKey = {};
   for (const key in k[Groupkey]) {
     const io = k[Groupkey][key];
@@ -139,8 +123,7 @@ if (typeof $argument !== "undefined" && $argument !== "") {
       AllKey[sp].push(valuep);
     }
   }
-  // console.log(AllKey);
-
+  
   // 计算平均
   const avgt = {};
   for (const sp in AllKey) {
@@ -159,7 +142,7 @@ if (typeof $argument !== "undefined" && $argument !== "") {
       minValue = value;
     }
   }
-  // console.log("次数: " + (Object.keys(k[Groupkey]).length));
+  
   const Pushs = "优选结果为: " + minKey +": " + avgt[minKey] + " ms"
   
   if (nowproxy === minKey) {
@@ -181,7 +164,6 @@ if (typeof $argument !== "undefined" && $argument !== "") {
     content: p + j,
   });
 })();
-
 function httpAPI(path = "", method = "GET", body = null) {
   return new Promise((resolve) => {
     $httpAPI(method, path, body, (result) => {
@@ -189,7 +171,6 @@ function httpAPI(path = "", method = "GET", body = null) {
     });
   });
 }
-
 function getin() {
   return Object.fromEntries(
     $argument
@@ -198,7 +179,6 @@ function getin() {
       .map(([k, v]) => [k, decodeURIComponent(v)])
   );
 }
-
 function reSpeed(x, y) {
   if (x > 1e6) {
     return Math.round(y * 0.3);
