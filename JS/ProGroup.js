56f35819a98ffea9ef83c69c277da18fe918f1f0
K <704496037@qq.com>
2023-07-09 02:07:45 +0800

    GroupAuto

--- JS/ProGroup.js
+++ JS/ProGroup.js
@@ -1,212 +1,25 @@
-/* 2023-07-08 23:32:11 修复策略组节点变更时需要删除缓存,现在不需要删除缓存了
+/* 2023-07-09 02:01:14
 作用: 根据 Surge httpApi 请求 (最近请求详情:速度、 参数指定的策略组:与其对应HASH节点延时、节点名)
 策略: 根据 api 返回的节点 (速度:持久化缓存非线性权重) 与 (延时:持久化缓存) 对节点进行优选, 不用担心费流量，因为通过httpApi取的最近请求里的速度数据
 参数: 
 
 group=          你的策略组名(需要填写手动选择的策略组select)
 tolerance=10    容差10ms 小于10ms则不切换节点
-timecache=18    缓存到期时间(小时) 或 超过66个数据会清理旧的数据
+timecache=18    缓存到期时间(小时) 或 超过40个数据会清理旧的数据
 push            加参数为开启通知, 不加参数则不通知
 
 #!name=GroupAuto
-#!desc=#!desc=优选节点: 根据 httpApi 返回的 (速度:持久化缓存非线性权重) (平均延时:持久化缓存)  (延迟最大与最小值的差) 优选
+#!desc=根据 api 返回的节点 (速度:持久化缓存非线性权重) 与 (延时:持久化缓存) 对节点进行优选
 
 [Panel]
-GroupAuto = script-name=GroupAuto,update-interval=6
+GroupAuto = script-name=GroupAuto,update-interval=3
 
 [Script]
-# 面板 运行 (面板与定时任务可同时存在 C代表次数;  节点后面数字代表 速度, 延迟, 延迟最大与最小值的差 三者的平均非线性取值)
-# 如遇到问题可以去:工具>脚本编辑器>左下角齿轮图标>$persistentStore 删除 KEY_GroupAuto持久化数据
-GroupAuto = type=generic,timeout=6,script-path=https://github.com/Keywos/rule/raw/main/JS/ProGroup.js,argument=tolerance=10&timecache=18&group=Proxy
-
-# 定时自动运行(可选) 5分钟一次
-# Cron_GroupAuto = type=cron, cronexp= "0/5 * * * *", timeout=6,wake-system=0,script-path=https://github.com/Keywos/rule/raw/main/JS/ProGroup.js,argument=tolerance=1&timecache=18&group=Proxy
+# 面板 运行 (面板与定时任务可同时存在)
+GroupAuto = type=generic,timeout=3,script-path=https://github.com/Keywos/rule/raw/main/JS/ProGroup.js,argument=group=VPS&tolerance=10&timecache=18
+# 定时自动运行 5分钟一次
+Cron_GroupAuto = type=cron, cronexp= "0/5 * * * *", timeout=5,script-path=https://github.com/Keywos/rule/raw/main/JS/ProGroup.js,argument=group=VPS&tolerance=1&timecache=18
 
 */
-let Groupkey = "VPS",tol = "10",th = "18",p = "", j = "", push = false;
-if (typeof $argument !== "undefined" && $argument !== "") {
-  const ins = getin("$argument");
-  Groupkey = ins.group ? ins.group : Groupkey;
-  th = ins.timecache ? ins.timecache : th;
-  tol = ins.tolerance ? ins.tolerance : tol;
-  push = ins.push ? ins.push : false;
-}
-(async () => {
-  // 获取请求记录详情过滤获取最高上下传速度
-  const Speed = await httpAPI("/v1/requests/recent");
-  const inoutMax = Speed.requests
-    .filter((o) =>/\(Proxy\)/.test(o.remoteAddress))
-    .map((i) => `${i.policyName}:${i.inMaxSpeed + i.outMaxSpeed}`);
-  const uValue = [...new Set(inoutMax)];
-  const avgts = {};
-  uValue.forEach((value) => {
-    const [name, num] = value.split(":");
-    if (!avgts[name]) {
-      avgts[name] = [parseInt(num), 1];
-    } else {
-      avgts[name][0] += parseInt(num);
-      avgts[name][1] += 1;
-    }
-  });
-  // 根据提取的速度调整优先级
-  const ioSpee = Object.entries(avgts).reduce(
-    (obj, [name, [sum, count]]) => {
-      const average = Math.round(sum / count);
-      obj[name] = average;
-      return obj;
-    },
-    {}
-  );
-  // 获取策略组内节点
-  const proxy = await httpAPI("/v1/policy_groups");
-  if (Object.keys(proxy).includes(Groupkey)){
-  // 提取 name 属性的值到一个新数组
-  const proxyName = proxy[Groupkey].map((obj) => obj.name);
 
-  // 请求测速选择的策略组
-  const testReq = await httpAPI("/v1/policy_groups/test", "POST", (body = { group_name: Groupkey }));
-  const nowproxy = testReq.available[0];
-
-  // 获取所有延时测速结果
-  const testGroup = await httpAPI("/v1/policies/benchmark_results");
-  // 提取 /v1/policy_groups 代理节点 中的 name 和 lineHash 的值
-  const resultsMs = proxy[Groupkey].map((i) => {
-    const lineHash = i.lineHash;
-    const name = i.name;
-    // 获取对应 /v1/policies/benchmark_results 的 lastTestScoreInMS为ms
-    const HashValue = testGroup[lineHash];
-    const HashMs = HashValue ? HashValue.lastTestScoreInMS : 998;
-    return { name, ms: HashMs };
-  });
-  //.filter(i => i.ms !== -1); //过滤测速失败的
-  //console.log(JSON.stringify(resultsMs, null, 2));
-  // 提取的测速结果
-  const kv = resultsMs.reduce((obj, { name, ms }) => {
-    obj[name] = ms;
-    return obj;
-  }, {});
-  
-  // 速度高的优选权重变高
-  for (let key in ioSpee) {
-    if (kv.hasOwnProperty(key)) {
-      kv[key] = reSpeed(Number(ioSpee[key]), Number(kv[key]));
-    }
-  }
-  const t = new Date().getTime();
-  // 读取存储的数据
-  const readData = $persistentStore.read("KEY_GroupAuto");
-  let k = readData ? JSON.parse(readData) : {};
-  k[Groupkey] = k[Groupkey] || {};
-  // 按个数 清理旧缓存
-  let timeNms = Object.keys(k[Groupkey]).length;
-  for (const t in k[Groupkey]) {
-    if (timeNms > 65) {
-      delete k[Groupkey][t];
-      timeNms--;
-    }
-  }
-  
-  let ag = Object.values(k[Groupkey])[0] ? String(Object.keys(Object.values(k[Groupkey])[0])) : "";
-  let ng = String(Object.keys(kv));
-  if (ag !== ng)k[Groupkey] = {};
-  k[Groupkey][t] = kv;
-  // 按时间戳 清理旧缓存
-  const GT = k[Groupkey];
-  const cleanT = Date.now();
-  Object.keys(GT).forEach((t) => {
-    const cha = cleanT - parseInt(t);
-    const hours = cha / (1000 * 60 * 60 * th);
-    hours > 1 && delete GT[t];
-  });
-  k[Groupkey] = GT;
-  $persistentStore.write(JSON.stringify(k), "KEY_GroupAuto");
-  //console.log(JSON.stringify(k[Groupkey],null,2));
-  const AllKey = {};
-  for (const key in k[Groupkey]) {
-    const io = k[Groupkey][key];
-    for (const sp in io) {
-      const score = io[sp];
-      if (!AllKey.hasOwnProperty(sp)) {
-        AllKey[sp] = [];
-      }
-      const np = parseInt(score);
-      const valuep = np < 0 ? 996 : np;
-      AllKey[sp].push(valuep);
-    }
-  }
-  
-  // 计算平均与最大最小差值
-  const avgt = {};
-  for (const sp in AllKey) {
-    const sa = AllKey[sp];
-    if (sa.length > 0) {
-      const sum = sa.reduce((a, b) => a + b, 0);
-      const average = Math.round(sum / sa.length);
-      const rangei = rangeMax(sa)
-      avgt[sp] = average + rangei;
-    }
-  }
-  
-  let minKey = null, minValue = Infinity;
-  for (const key in avgt) {
-    const value = avgt[key];
-    if (value < minValue) {
-      minKey = key;
-      minValue = value;
-    }
-  }
-  
-  let Pushs = "";
-  if (nowproxy === minKey) {
-		Pushs = "继承优选: " + minKey +": " + avgt[minKey]
-   
-  } else if (avgt[nowproxy] - avgt[minKey] > tol) {
-    await httpAPI("/v1/policy_groups/select","POST",(body = { group_name: Groupkey, policy: minKey }));
-    Pushs = "优选成功: " + minKey +": " + avgt[minKey]
-  } else {
-    Pushs = "容差内优选: " + nowproxy +": " + avgt[nowproxy]
-  }
-  console.log(Pushs);
-  const te = new Date(t),he = String(te.getHours()).padStart(2, '0'),me = String(te.getMinutes()).padStart(2, '0');
-  (push) && ($notification.post("",Pushs,""));
-  $done({
-    title: "GroupAuto " + he +":"+ me +" "+ Object.keys(k[Groupkey]).length+"C: "+ Groupkey +'\''+ Object.keys(proxy[Groupkey]).length,
-    content: Pushs,
-  });
-  } else {
-	console.log("group参数未输入正确的策略组")
-	$done({
-    title: "GroupAuto",
-    content: "group参数未输入正确的策略组",
-  });
- }
-})();
-function httpAPI(path = "", method = "GET", body = null) {
-  return new Promise((resolve) => {
-    $httpAPI(method, path, body, (result) => {
-      resolve(result);
-    });
-  });
-}
-function getin() {
-  return Object.fromEntries(
-    $argument
-      .split("&")
-      .map((item) => item.split("="))
-      .map(([k, v]) => [k, decodeURIComponent(v)])
-  );
-}
-function reSpeed(x, y) {
-  if (x > 1e6) {
-    return Math.round(y * 0.3);
-  } else {
-    const ob = 0.99 * Math.exp(-x / 2e6);
-    return Math.round(y * ob);
-  }
-}
-
-function rangeMax(e) {
-  const max = Math.max(...e),
-  min = Math.min(...e);
-  return max - min;
-}
+let Groupkey="VPS",tol="10",th="18",push=false,timeout=9900;if(typeof $argument!=="undefined"&&$argument!==""){const e=getin("$argument");Groupkey=e.group?e.group:Groupkey;th=e.timecache?e.timecache:th;tol=e.tolerance?e.tolerance:tol;push=e.push?e.push:false;console.log(e)}(async()=>{const e=await httpAPI("/v1/requests/recent");let t,o,n;if(!e){console.log("kv");$done(bkey)}else{const o=e.requests.filter((e=>/\(Proxy\)/.test(e.remoteAddress))).map((e=>`${e.policyName}:${e.inMaxSpeed+e.outMaxSpeed}`));const n=[...new Set(o)];const r={};n.forEach((e=>{const[t,o]=e.split(":");if(!r[t]){r[t]=[parseInt(o),1]}else{r[t][0]+=parseInt(o);r[t][1]+=1}}));t=Object.entries(r).reduce(((e,[t,[o,n]])=>{const r=Math.round(o/n);e[t]=r;return e}),{})}const r=await httpAPI("/v1/policy_groups");if(!r){$done(bkey)}else{if(!Object.keys(r).includes(Groupkey)){$done({title:"GroupAuto",content:"group参数未输入正确的策略组"})}}const s=await httpAPI("/v1/policy_groups/test","POST",body={group_name:Groupkey});if(!s){$done(bkey)}else{o=s.available[0]}const c=await httpAPI("/v1/policies/benchmark_results");if(!c){console.log("有延时测速结果");$done(bkey)}else{n=r[Groupkey].map((e=>{const t=e.lineHash;const o=e.name;const n=c[t];const r=n?n.lastTestScoreInMS:998;return{name:o,ms:r}}));const e=n.reduce(((e,{name:t,ms:o})=>{e[t]=o;return e}),{});for(let o in t){if(e.hasOwnProperty(o)){e[o]=reSpeed(Number(t[o]),Number(e[o]))}}const s=(new Date).getTime();const u=$persistentStore.read("KEY_GroupAuto");let p=u?JSON.parse(u):{};p[Groupkey]=p[Groupkey]||{};let i=Object.keys(p[Groupkey]).length;for(const e in p[Groupkey]){if(i>65){delete p[Groupkey][e];i--}}let a=Object.values(p[Groupkey])[0]?String(Object.keys(Object.values(p[Groupkey])[0])):"";let l=String(Object.keys(e));if(a!==l)p[Groupkey]={};p[Groupkey][s]=e;const y=p[Groupkey];const h=Date.now();Object.keys(y).forEach((e=>{const t=h-parseInt(e);const o=t/(1e3*60*60*th);o>1&&delete y[e]}));p[Groupkey]=y;$persistentStore.write(JSON.stringify(p),"KEY_GroupAuto");const f={};for(const e in p[Groupkey]){const t=p[Groupkey][e];for(const e in t){const o=t[e];if(!f.hasOwnProperty(e)){f[e]=[]}const n=parseInt(o);const r=n<0?996:n;f[e].push(r)}}const k={};for(const e in f){const t=f[e];if(t.length>0){const o=t.reduce(((e,t)=>e+t),0);const n=Math.round(o/t.length);const r=rangeMax(t);k[e]=n+r}}let g=null,m=Infinity;for(const e in k){const t=k[e];if(t<m){g=e;m=t}}let d="";if(o===g){d="继承优选: "+g+": "+k[g]}else if(k[o]-k[g]>tol){await httpAPI("/v1/policy_groups/select","POST",body={group_name:Groupkey,policy:g});d="优选成功: "+g+": "+k[g]}else{d="容差内优选: "+o+": "+k[o]}console.log(d);push&&$notification.post("",d,"");const G=new Date(s),b=String(G.getHours()).padStart(2,"0"),S=String(G.getMinutes()).padStart(2,"0");$done({title:"GroupAuto "+b+":"+S+" "+Object.keys(p[Groupkey]).length+"C: "+Groupkey+"'"+Object.keys(r[Groupkey]).length,content:d})}})();function httpAPI(e="",t="GET",o=null){return new Promise(((n,r)=>{const s=new Promise(((e,t)=>{setTimeout((()=>{t("");n("")}),timeout)}));const c=new Promise((n=>{$httpAPI(t,e,o,n)}));Promise.race([c,s]).then((e=>{n(e)})).catch((e=>{r(e)}))}))}function getin(){return Object.fromEntries($argument.split("&").map((e=>e.split("="))).map((([e,t])=>[e,decodeURIComponent(t)])))}function reSpeed(e,t){if(e>1e6){return Math.round(t*.3)}else{const o=.99*Math.exp(-e/2e6);return Math.round(t*o)}}function rangeMax(e){const t=Math.max(...e),o=Math.min(...e);return t-o}var bkey={title:"GroupAuto",content:"超过Surge httpApi 10秒限制"};
