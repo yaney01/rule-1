d18e3d5589f82acf3182dd158517fcde800eba66
K <104524788+Keywos@users.noreply.github.com>
2023-07-08 12:46:56 +0800

    tk.js

--- loon/tk.js
+++ loon/tk.js
@@ -4,19 +4,8 @@ if(loc == "inkey"){
   inkeys = $persistentStore.read("手动输入地区代码[可选]");
   loc = inkeys
 }
-/*
-if (/(tnc|dm).+?\.com.+/.test(url)) {
-  url = url.replace(/\/\?.+/g,"\?");
-  const response = {
-    status: 302,
-    headers: {Location: url},
-  };
-  $done({response});
-} 
-*/
 if (/_region=CN&|&mcc_mnc=4/.test(url)) {
-  url = url.replace(/_region=CN&/g,`_region=${loc}&`)
-            .replace(/&mcc_mnc=4/g,"&mcc_mnc=2");
+  url = url.replace(/_region=CN&/g,`_region=${loc}&`).replace(/&mcc_mnc=4/g,"&mcc_mnc=2");
   const response = {
     status: 307,
     headers: {Location: url},
