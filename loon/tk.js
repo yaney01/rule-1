1803ba6773a04f65756054aeba28d9e34d720a6d
K <104524788+Keywos@users.noreply.github.com>
2023-07-09 17:05:39 +0800

    tk.js

--- loon/tk.js
+++ loon/tk.js
@@ -4,7 +4,7 @@ if(loc == "inkey"){
   inkeys = $persistentStore.read("手动输入地区代码[可选]");
   loc = inkeys
 }
-if (/(tnc|dm).+?\.com.*?\?.+/.test(url)) {
+if (/^https?:\/\/(tnc|dm).+\.[^\s/]+\.com\/\w+/v\d/?/.test(url)) {
   url = url.replace(/\/\?/g,'');
   const response = {
     status: 302,
