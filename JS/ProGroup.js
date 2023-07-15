ad963d9ba1ee8115da20b45385413f52c3ba9c42
K <704496037@qq.com>
2023-07-15 23:49:44 +0800

    [v]

--- JS/ProGroup.js
+++ JS/ProGroup.js
@@ -1,4 +1,4 @@
-/* 2023-07-15 14:09:12 新增参数 timeout
+/* 2023-07-15 23:49:42
 作用: 
 · 如果策略组 节点变更 会重新缓存结果 重新取值
 · 如果有节点偶尔ping不通 那么大概率不会选中他 
@@ -36,18 +36,10 @@ GroupAuto = type=generic,timeout=3,script-path=https://github.com/Keywos/rule/ra
 let Groupkey = "VPS", tol = "10", th = "18", push = false, timeout = 6000, icons= "",icolor="";
 if (typeof $argument !== "undefined" && $argument !== "") {
   const ins = getin("$argument");
-  Groupkey = ins.group || Groupkey;
-  th = ins.timecache || th;
-  tol = ins.tolerance || tol;
-  push = ins.push || false;
-  icons = ins.icon || icons;
-  icolor = ins.color || icolor;
-  if (ins.timeout) {
-    timeout = Math.max(100, Math.min(9900, ins.timeout));
-  }
-	//console.log(ins)
+  Groupkey = ins.group || Groupkey;th = ins.timecache || th;tol = ins.tolerance || tol;
+  push = ins.push || false;icons = ins.icon || icons;icolor = ins.color || icolor;
+  if (ins.timeout) {timeout = Math.max(100, Math.min(9900, ins.timeout));}
 }
-
 
 function httpAPI(path = "", method = "GET", body = null) {
   return new Promise((resolve, reject) => {
@@ -88,151 +80,126 @@ function reSpeed(x, y) {
 }
 
 (async () => {
-	let NP,resMS,AK = {},Pushs = "";
-  const Protest = await httpAPI("/v1/policy_groups/test","POST",(body = { group_name: Groupkey }));
-	if (Protest){
-		console.log(Protest)
-	const Npolicy = await httpAPI("/v1/policy_groups/select?group_name="+encodeURIComponent(Groupkey));
-	NP = Npolicy.policy
-console.log(NP)
-  const proxy = await httpAPI("/v1/policy_groups");
-  if (!Object.keys(proxy).includes(Groupkey)) {
-    $done({title: "GroupAuto",content: "group参数未输入正确的策略组",});
-  }
-
-  const Sproxy = await httpAPI("/v1/traffic");
-  const { connector } = Sproxy;
-  const IOM = {}; // inMaxSpeed outMaxSpeed Max
-  Object.keys(connector).forEach((key) => {
-    const { inMaxSpeed, outMaxSpeed, lineHash } = connector[key];
-    if (lineHash && inMaxSpeed) {
-      IOM[lineHash] = inMaxSpeed + outMaxSpeed;
-    }
-  });
-
-  const testGroup = await httpAPI("/v1/policies/benchmark_results");
-  // /v1/policy_groups  中的 name 和 lineHash 
-  resMS = proxy[Groupkey].map((i) => {
-    const lineHash = i.lineHash;
-    const name = i.name;
-    //  /v1/policies/benchmark_results 的 lastTestScoreInMS 为 ms
-    let HashValue = testGroup[lineHash];
-    if (HashValue.lastTestScoreInMS === -1) {HashValue.lastTestScoreInMS = 996;}
-    const HashMs = HashValue ? HashValue.lastTestScoreInMS : 998;
-    return { name, ms: HashMs, lineHash };
-  });
-  resMS.forEach((i) => {
-    var lineHash = i.lineHash;
-    if (lineHash in IOM) {
-      i.se = IOM[lineHash];
-    } else {
-      i.se = "0";
-    }
-    delete i.lineHash;
-  });
-
-  const t = new Date().getTime();
-  const readData = $persistentStore.read("KEY_Group_Auto");
-	console.log("5")
-  let k = readData ? JSON.parse(readData) : {};
-	console.log("6")
-  k[Groupkey] = k[Groupkey] || {};
-  let timeNms = Object.keys(k[Groupkey]).length;
-  for (const t in k[Groupkey]) {
-    if (timeNms > 65) {
-      delete k[Groupkey][t];
-      timeNms--;
-    }
-  }
-	console.log(JSON.stringify(resMS, null, 2));
-	//console.log(Object.values(k[Groupkey])[0])
-  
-
-   if (Object.values(k[Groupkey])[0]) {
-    const groupValues = Object.values(k[Groupkey])[0];
-    
-    if (groupValues.some((i) => !resMS.some((e) => e.name === i.name)) ||
-        resMS.some((i) => !groupValues.some((e) => e.name === i.name))) {
-      k[Groupkey] = {};
-      console.log("数据变更，清理缓存");
-    }
-}
-
-  k[Groupkey][t] = resMS;
-  const h = Date.now();
-  Object.keys(k).forEach((ig) => {
-    const y = k[ig];
-    Object.keys(y).forEach((e) => {
-      const t = h - parseInt(e);
-      const o = t / (36e5 * th);
-      if (o > 1) {
-        delete y[e];
+  try {
+    let NP,resMS,Pushs = "",newp="",CC ="",AK = {};
+    const proxy = await httpAPI("/v1/policy_groups");
+      if (!Object.keys(proxy).includes(Groupkey)) {
+        $done({title: "GroupAuto",content: "group参数未输入正确的策略组",});
       }
-    });
-  });
-  $persistentStore.write(JSON.stringify(k), "KEY_Group_Auto");
-	console.log(k)
-  Object.values(k[Groupkey]).forEach((arr) => {
-    arr.forEach(({ name, ms, se }) => {
-      if (!AK[name]) {
-        AK[name] = { sum: Number(ms), count: 1, sek: Number(se) };
+    const Protest = await httpAPI("/v1/policy_groups/test","POST",(body = { group_name: Groupkey }));
+      if (Protest){ 
+        NP = Protest.available[0];
       } else {
-        AK[name].sum += Number(ms);
-        AK[name].sek += Number(se);
-        AK[name].count++;
+        const Npolicy = await httpAPI(`/v1/policy_groups/select?group_name=${encodeURIComponent(Groupkey)}`);
+        NP = Npolicy.policy;
       }
-    });
-  });
 
-  Object.keys(AK).forEach((name) => {
-    const { sum, count, sek } = AK[name];
-    AK[name] = {sum, count, sek: Number(sek) / Number(count),
-      avg: reSpeed(Number(sek), Number(sum) / Number(count)),
-    };
-  });
+    const Sproxy = await httpAPI("/v1/traffic");
+      const { connector } = Sproxy;
+      const IOM = {}; // inMaxSpeed outMaxSpeed Max
+      Object.keys(connector).forEach((key) => {
+        const { inMaxSpeed, outMaxSpeed, lineHash } = connector[key];
+        if (lineHash && inMaxSpeed) {
+          IOM[lineHash] = inMaxSpeed + outMaxSpeed;
+        }
+      });
 
-  const AVGT = Object.fromEntries(
-    Object.entries(AK).map(([key, value]) => [key, value.avg])
-  );
-  console.log(AVGT);
-  let minKey = null,
-    minValue = Infinity;
-  for (const key in AVGT) {
-    const value = AVGT[key];
-    if (value < minValue) {
-      minKey = key;
-      minValue = value;
+    const testGroup = await httpAPI("/v1/policies/benchmark_results");
+      // /v1/policy_groups  中的 name 和 lineHash 
+      resMS = proxy[Groupkey].map((i) => {
+        const lineHash = i.lineHash;
+        const name = i.name;
+        //  /v1/policies/benchmark_results 的 lastTestScoreInMS 为 ms
+        let HashValue = testGroup[lineHash];
+        if (HashValue.lastTestScoreInMS === -1) {HashValue.lastTestScoreInMS = 996;}
+        const HashMs = HashValue ? HashValue.lastTestScoreInMS : 998;
+        return { name, ms: HashMs, lineHash };
+      });
+      resMS.forEach((i) => {
+        var lineHash = i.lineHash;
+        if (lineHash in IOM) {
+          i.se = IOM[lineHash];
+        } else {
+          i.se = "0";
+        }
+        delete i.lineHash;
+      });
+
+    const tc = new Date().getTime();
+    const readData = $persistentStore.read("KEY_Group_Auto");
+    let k = readData ? JSON.parse(readData) : {};
+    k[Groupkey] = k[Groupkey] || {};
+    let timeNms = Object.keys(k[Groupkey]).length;
+    for (const t in k[Groupkey]) {
+      if (timeNms > 65) {delete k[Groupkey][t];timeNms--;}
     }
+    console.log(JSON.stringify(resMS, null, 2));
+
+    if (Object.values(k[Groupkey])[0]) {
+      const groupValues = Object.values(k[Groupkey])[0];
+      if (groupValues.some((i) => !resMS.some((e) => e.name === i.name)) || resMS.some((i) => !groupValues.some((e) => e.name === i.name))) {
+        k[Groupkey] = {};newp="\n数据变更, 清理缓存 !";
+      }
+    }
+    k[Groupkey][tc] = resMS;
+    Object.keys(k).forEach((ig) => {const y = k[ig];
+      Object.keys(y).forEach((e) => {const t = tc - parseInt(e);const o = t/(36e5 * th);if (o>1) {delete y[e];}});
+    });
+    $persistentStore.write(JSON.stringify(k), "KEY_Group_Auto");
+
+    Object.values(k[Groupkey]).forEach((arr) => {
+      arr.forEach(({ name, ms, se }) => {
+        if (!AK[name]) {
+          AK[name] = { sum: Number(ms), count: 1, sek: Number(se) };
+        } else {
+          AK[name].sum += Number(ms);
+          AK[name].sek += Number(se);
+          AK[name].count++;
+        }
+      });
+    });
+
+    Object.keys(AK).forEach((name) => {
+      const { sum, count, sek } = AK[name];
+      AK[name] = {sum, count, sek: Number(sek) / Number(count),
+        avg: reSpeed(Number(sek), Number(sum) / Number(count)),
+      };
+    });
+
+    const AVGT = Object.fromEntries(
+      Object.entries(AK).map(([key, value]) => [key, value.avg])
+    );
+    // console.log(AVGT);
+    let minKey = null,
+      minValue = Infinity;
+    for (const key in AVGT) {
+      const value = AVGT[key];
+      if (value < minValue) {
+        minKey = key;
+        minValue = value;
+      }
+    }
+    if (NP === minKey) {
+      Pushs ="继承: " +minKey +": "+BtoM(AK[minKey]["sek"]) +" "+AVGT[minKey];
+      CC =AK[minKey]["count"] +"C"
+    } else if (AVGT[NP] - AVGT[minKey] > tol) {
+      await httpAPI("/v1/policy_groups/select","POST",(body = { group_name: Groupkey, policy: minKey }));
+        Pushs ="优选: " +minKey +": "+BtoM(AK[minKey]["sek"]) +" "+AVGT[minKey];
+        CC = AK[minKey]["count"] +"C"
+    } else {
+      Pushs ="容差:" +NP +": "+BtoM(AK[NP]["sek"]) +" "+AVGT[NP];
+      CC = AK[NP]["count"] +"C"
+    }
+    console.log(newp+Pushs+" "+CC);
+    push && $notification.post("", Pushs, "");
+    $done({
+      title:"XGroup: "+Groupkey +"‘"+Object.keys(proxy[Groupkey]).length+" "+CC,
+      content: Pushs+newp,
+      icon: icons,
+      'icon-color': icolor
+    });
+  } catch (error) {
+    console.error(error);
+    $done()
   }
-	console.log(minKey)
-	let CC =""
-  if (NP === minKey) {
-    Pushs ="继承: " +minKey +": "+BtoM(AK[minKey]["sek"]) +" "+AVGT[minKey];
-		CC =AK[minKey]["count"] +"C"
-  } else if (AVGT[NP] - AVGT[minKey] > tol) {
-    await httpAPI("/v1/policy_groups/select","POST",(body = { group_name: Groupkey, policy: minKey }));
-    Pushs ="优选: " +minKey +": "+BtoM(AK[minKey]["sek"]) +" "+AVGT[minKey];
-		CC = AK[minKey]["count"] +"C"
-  } else {
-    Pushs ="容差:" +NP +": "+BtoM(AK[NP]["sek"]) +" "+AVGT[NP];
-		CC = AK[NP]["count"] +"C"
-  }
-  console.log(Pushs);
-  push && $notification.post("", Pushs, "");
-  const te = new Date(t),
-    he = te.getHours(),
-    me = te.getMinutes();
-  $done({
-    title:"XGroup: "+Groupkey +"‘"+Object.keys(proxy[Groupkey]).length+" "+CC,
-    content: Pushs,
-		icon: icons,
-    'icon-color': icolor
-  });
-	} else {
-		 $done({
-    title:"错误",
-    content: "",
-  });
-		
-	}
 })();
