e6c873f97ff70d3973b7e0779e89ef3467f96548
K <104524788+Keywos@users.noreply.github.com>
2023-07-16 20:02:35 +0800

    6

--- JS/ProGroup.js
+++ JS/ProGroup.js
@@ -27,9 +27,9 @@ GroupAuto = script-name=GroupAuto,update-interval=3
 
 [Script]
 # 面板 运行 (面板与定时任务可同时存在)
-GroupAuto = type=generic,timeout=3,script-path=https://github.com/Keywos/rule/raw/main/JS/ProGroup.js,argument=group=VPS&tolerance=10&timecache=18&color=#6699FF&icon=speedometer
+GroupAuto = type=generic,timeout=3,script-path=https://github.com/Keywos/rule/raw/main/JS/ProGroup.js,argument=group=VPS&tolerance=15&timecache=18&color=#6699FF&icon=speedometer
 # 定时自动运行(可选需取消#注释) 30分钟一次,每天2到7点不运行
-# Cron_GroupAuto = type=cron, cronexp= "0/30 0,1,7-23 * * *", timeout=10,wAllKeye-system=0,script-path=https://raw.githubusercontent.com/Keywos/rule/main/JS/ProGroup.js, argument=tolerance=10&timecache=18&group=Proxy
+# Cron_GroupAuto = type=cron, cronexp= "0/30 0,1,7-23 * * *", timeout=15,wAllKeye-system=0,script-path=https://raw.githubusercontent.com/Keywos/rule/main/JS/ProGroup.js, argument=tolerance=10&timecache=18&group=Proxy
 
 */
 
