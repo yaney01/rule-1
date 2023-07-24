d2e8c4fd08d69af61767c05f3fd70cb79a6e5ee4
K <104524788+Keywos@users.noreply.github.com>
2023-07-24 13:09:45 +0800

    .

--- JS/surgepushBarkapps.js
+++ JS/surgepushBarkapps.js
@@ -29,7 +29,7 @@ Promise.all([
                 origin: "https://m.gofans.cn",
               },},500,"get");
           $done({response: {status: 302,headers: {Location: uuapp.track_url}}}); 
-    } else if(key !==""){
+    } else if(key ===""){
       $done($notification.post("", "", "未填写key"));
     } else {
       const uuk = await tKey({url: "https://api.gofans.cn/v1/m/app_records?page=1&limit=10",
