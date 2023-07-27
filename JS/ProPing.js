d0241902261805ad7b9f8aa9a436f9c8ee03aa8a
K <704496037@qq.com>
2023-07-27 17:02:47 +0800

    [+] timeout

--- JS/ProPing.js
+++ JS/ProPing.js
@@ -1,6 +1,9 @@
 const cnurl = "http://connectivitycheck.platform.hicloud.com/generate_204";
 const cfurl = "http://cp.cloudflare.com/generate_204";
-let num = "7",icons="",icolor="";
+let num = "7",
+  icons = "barometer",
+  icolor = "#80A0BF",
+  repin = 0;
 if (typeof $argument !== "undefined" && $argument !== "") {
   const ins = getin("$argument");
   num = ins.num || num;
@@ -8,36 +11,41 @@ if (typeof $argument !== "undefined" && $argument !== "") {
   icolor = ins.color || icolor;
 }
 (async () => {
-	try {
-	  let cn = [],cf = [];
-	  for (let e = 0; e < 2; e++) {
-		const u = await http(cnurl);
-		const k = parseFloat(u);
-		cn.push(k);
-  
-		const e = await http(cfurl);
-		const n = parseFloat(e);
-		cf.push(n);
-	  }
-	  const d = ((cn[0] + cn[1]) / 2) | 0;
-	  const e = ((cf[0] + cf[1]) / 2) | 0;
-  
-	  const n = saK(d, e);
-	  const od = ptoG(n["CN"]);
-	  const op = ptoG(n["CF"]);
-	  $done({
-		title: `CN: ${d}    ➟    CF: ${e}`,
-		content: od + "." + op,
-		icon: icons,
-        'icon-color': icolor
-	  });
-	} catch (i) {
-	  const err = 'Feedback @𝙺𝚎𝚢 !! ';
-	  console.log(err+i.message)
-	  $done({title:err, content:i.message})
+  try {
+    let cn = [], cf = [], d, e;
+    while (repin < 2) {
+      const u = await http(cnurl);
+      const k = parseFloat(u);
+      cn.push(k);
+
+      const eValue = await http(cfurl);
+      const n = parseFloat(eValue);
+      cf.push(n);
+      repin++;
+    }
+	if ( repin === 2 ){
+		d = Math.floor((cn[0] + cn[1]) / 2);
+		e = Math.floor((cf[0] + cf[1]) / 2);
+	} else {
+		d = cn[0];
+		e = cf[0];
 	}
-  })().finally(() => $done());
-  
+    const n = saK(d, e);
+    const od = ptoG(n["CN"]);
+    const op = ptoG(n["CF"]);
+    $done({
+      title: `CN: ${d}    ➟    CF: ${e}`,
+      content: od + "." + op,
+      icon: icons,
+      "icon-color": icolor,
+    });
+  } catch (i) {
+    const err = "Feedback @𝙺𝚎𝚢 !! ";
+    console.log(err + i.message);
+    $done({ title: err, content: i.message });
+  }
+})().finally(() => $done());
+
 function saK(d, t) {
   let k;
   try {
@@ -52,10 +60,69 @@ function saK(d, t) {
 }
 function getin() {
   return Object.fromEntries(
-    $argument.split("&").map((i) => i.split("="))
-    .map(([k, v]) => [k, decodeURIComponent(v)])
+    $argument
+      .split("&")
+      .map((i) => i.split("="))
+      .map(([k, v]) => [k, decodeURIComponent(v)])
   );
 }
-function getArr(x, l) {return Array(l).fill(x);}
-async function http(url) {return new Promise((t) => {let e = Date.now();$httpClient.get(url, () => {let n = Date.now();t(n - e);});});}
-function ptoG(t) {const e = 10;let n;n = Math.max(...t);let o = n;if (n < 70) {o += 200;} else if (n < 150) {o += 150;} else if (n < 250) {o += 100;} else if (n < 400) {o += 2;} else {o = 410;}const r = t.map((t) => {let n = (t - e) / (o - e);if (n > 1) {n = 1;}const r = Math.floor(n * 6) + 9601;if (r > 9607) {return "▇";} else if (r < 9601) {return "▁";} else {return String.fromCharCode(r);}}).join("");return r;}
+function getArr(x, l) {
+  return Array(l).fill(x);
+}
+async function http(url) {
+  return new Promise((resolve, reject) => {
+    let e = Date.now();
+    const timeoutPromise = new Promise((_, reject) => {
+      setTimeout(() => {
+        repin++;
+        reject("");
+        resolve("2e3");
+      }, 1900);
+    });
+    const reqPromise = new Promise((resolve) => {
+      $httpClient.get(url, resolve);
+    });
+    Promise.race([reqPromise, timeoutPromise])
+      .then((i) => {
+        resolve(Date.now() - e);
+      })
+      .catch((error) => {
+        reject(error);
+        resolve("1e4");
+      });
+  });
+}
+function ptoG(t) {
+  const e = 10;
+  let n;
+  n = Math.max(...t);
+  let o = n;
+  if (n < 70) {
+    o += 200;
+  } else if (n < 150) {
+    o += 150;
+  } else if (n < 250) {
+    o += 100;
+  } else if (n < 400) {
+    o += 2;
+  } else {
+    o = 410;
+  }
+  const r = t
+    .map((t) => {
+      let n = (t - e) / (o - e);
+      if (n > 1) {
+        n = 1;
+      }
+      const r = Math.floor(n * 6) + 9601;
+      if (r > 9607) {
+        return "▇";
+      } else if (r < 9601) {
+        return "▁";
+      } else {
+        return String.fromCharCode(r);
+      }
+    })
+    .join("");
+  return r;
+}
