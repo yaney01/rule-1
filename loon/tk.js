70f1fbc70d3552a4dfb5969c38cc3c5e3974d3d4
K <104524788+Keywos@users.noreply.github.com>
2023-11-19 07:11:37 +0800

    [v]

--- loon/tk.js
+++ loon/tk.js
@@ -1,5 +1,5 @@
 let keyus={日本: "JP", 韩国: "KR", 英国:"UK", 美国:"US", 台湾:"TW", 香港:"HK", 新加坡:"SG", 法国:"FR", 马来西亚:"MY", 菲律宾:"PH", 泰国:"TH", 手动输入:"inkey"},url = $request.url,lk = "",loc = "";
-if (typeof $argument !== 'undefined' && $argument !== "") {loc = $argument;} else {lk = $persistentStore.read("TikTok解锁地区");loc = keyus[lk] || "KR";if(loc == "inkey"){inkeys = $persistentStore.read("手动输入地区代码[可选]");loc = inkeys;}};
+if (typeof $argument !== 'undefined' && $argument !== "") {loc = $argument ?? "KR";} else {lk = $persistentStore.read("TikTok解锁地区");loc = keyus[lk] || "KR";if(loc == "inkey"){inkeys = $persistentStore.read("手动输入地区代码[可选]");loc = inkeys;}};
 
 if (/(tnc|dm).+\.[^\/]+\.com\/\w+\/v\d\/\?/.test(url)) {
   url = url.replace(/\/\?/g,'');
