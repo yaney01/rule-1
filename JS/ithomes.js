72092dea549b4df4a2bb82c6c35c8bb78c2e3571
K <704496037@qq.com>
2023-11-23 21:23:46 +0800

    [v]

--- JS/ithomes.js
+++ JS/ithomes.js
@@ -1,22 +1,2 @@
-const isLoon =typeof $loon !== 'undefined'
-let lbt=true, zd=true
-if (isLoon){
-	lbt = $persistentStore.read("去除轮播图") === "开启";
-    zd = $persistentStore.read("去除置顶") === "开启";
-}
-let i = JSON.parse($response.body);
-if(lbt){
-    i.data.list = i.data.list.filter(function(item) {
-    return item.feedType !== 10002;  //轮播
-    });
-}
-if(zd){
-    i.data.list = i.data.list.filter(function(item) {
-    return item.feedType !== 10003;  //置顶
-    });
-}
-i.data.list = i.data.list.filter(item => {
-  return item.feedType !== 10000 || (item.feedContent.smallTags[0]?.text === null || !item.feedContent.smallTags[0].text.includes("广告"));
-});
-
-$done({body: JSON.stringify(i)});
+// @timestamp thenkey 2023-11-23 21:20:16
+const e="undefined"!=typeof $loon;let t=!0,n=!0;e&&(t="开启"===$persistentStore.read("去除轮播图"),n="开启"===$persistentStore.read("去除置顶"));let s=JSON.parse($response.body);t&&(s.data.list=s.data.list.filter((function(e){return 10002!==e.feedType}))),n&&(s.data.list=s.data.list.filter((function(e){return 10003!==e.feedType}))),s.data.list=s.data.list.filter((e=>1e4!==e.feedType||null===e.feedContent.smallTags[0]?.text||!e.feedContent.smallTags[0].text.includes("广告"))),$done({body:JSON.stringify(s)});
