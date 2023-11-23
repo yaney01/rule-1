7632ca3ce8dca74d09dbb6c2551ee30065c0ffe9
K <704496037@qq.com>
2023-11-24 05:07:00 +0800

    [v]

--- JS/dy.js
+++ JS/dy.js
@@ -1,6 +1,12 @@
 /**
- * 抖音 = type=http-response,pattern=^https:\/\/((tnc|dm).+\.[^\/]+\.com\/\w+\/v\d\/\?|aweme\.snssdk\.com\/aweme\/homepage\/render\/\?),requires-body=1,script-path=https://github.com/Keywos/rule/raw/main/JS/dy.js,max-size=0
- * https://tnc11-aliec2.zijieapi.com/get_domains/v5/?
+ * 
+[Script]
+抖音 = type=http-response,pattern=^https:\/\/((tnc|dm).+\.[^\/]+\.com\/\w+\/v\d\/\?|aweme\.snssdk\.com\/aweme\/homepage\/render\/\?),requires-body=1,script-path=https://github.com/Keywos/rule/raw/main/JS/dy.js,max-size=0
+
+[MITM]
+hostname = %APPEND% *.zijieapi.com, aweme.snssdk.com
+
+// https://tnc11-aliec2.zijieapi.com/get_domains/v5/?
  */
 
 let i = JSON.parse($response.body),
