e23b832b0b050e61259195f549bfb417c6e6c894
K <704496037@qq.com>
2023-11-27 14:09:02 +0800

    [v]

--- JS/vv.js
+++ JS/vv.js
@@ -1,13 +1,25 @@
-// 转自 @suiyuran
+// 转自 适配qx@key @suiyuran
+const isQX = typeof $task !== "undefined";
 let url = $request.url;
-let hasUid = (url) => url.includes("uid");
+let hasUid = (url) => url.includes("vvebo_uid");
 let getUid = (url) => (hasUid(url) ? url.match(/uid=(\d+)/)[1] : undefined);
 if (url.includes("users/show")) {
-  $persistentStore.write(getUid(url), "uid");
+  if (isQX) {
+    $prefs.setValueForKey(getUid(url), "vvebo_uid");
+  } else {
+    $persistentStore.write(getUid(url), "vvebo_uid");
+  }
   $done({});
 } else if (url.includes("statuses/user_timeline")) {
-  let uid = getUid(url) || $persistentStore.read("uid");
-  url = url.replace("statuses/user_timeline", "profile/statuses/tab").replace("max_id", "since_id");
+  let uid = getUid(url);
+  if (isQX) {
+    uid = $prefs.valueForKey("vvebo_uid");
+  } else {
+    uid = $persistentStore.read("vvebo_uid");
+  }
+  url = url
+    .replace("statuses/user_timeline", "profile/statuses/tab")
+    .replace("max_id", "since_id");
   url = url + `&containerid=230413${uid}_-_WEIBO_SECOND_PROFILE_WEIBO`;
   $done({ url });
 } else if (url.includes("profile/statuses/tab")) {
@@ -18,7 +30,9 @@ if (url.includes("users/show")) {
     .filter((card) => card.card_type === 9)
     .map((card) => card.mblog);
   let sinceId = data.cardlistInfo.since_id;
-  $done({ body: JSON.stringify({ statuses, since_id: sinceId, total_number: 100 }) });
+  $done({
+    body: JSON.stringify({ statuses, since_id: sinceId, total_number: 100 }),
+  });
 } else {
   $done({});
 }
