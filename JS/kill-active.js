38809aefe8ed9cf6f8a72fd45f713d88484dfbf7
K <104524788+Keywos@users.noreply.github.com>
2023-12-11 03:49:57 +0800

    [v]

--- JS/kill-active.js
+++ JS/kill-active.js
@@ -1,37 +1,61 @@
 // 改自 https://raw.githubusercontent.com/xream/scripts/main/surge/modules/kill-active-requests/index.sgmodule
-let result = {},aid = [],le=0;
+let result = {}, aid = [], le = 0;
+const noPanel = typeof $input == "undefined";
 !(async () => {
   const { requests = [] } = (await httpAPI("/v1/requests/active", "GET")) || {};
-  for await (const { id,URL } of requests) {
-		aid.push(URL)
+  for await (const { id, URL } of requests) {
+    if (noPanel) {
+      aid.push(URL);
+    } else {
+      const domain = URL.match(/\.([^:/]+)\./)[1] ?? "";
+      aid.push(domain.replace(/\.(com|net)$/, ""));
+      console.log(URL);
+    }
     await httpAPI("/v1/requests/kill", "POST", { id });
   }
-	le = Math.max(requests.length - 1, 0);
+  le = Math.max(requests.length - 1, 0);
+  let xc = "";
+  if (noPanel) {
+    xc = aid.slice(0, -1).join("\n");
+  } else {
+    xc = le === 0 ? "无活跃请求" : aid.slice(0, -1).join(", ");
+  }
   result = {
-    title: "打断活跃请求数: "+ le,
-    content: aid.slice(0,-1).join("\n"),
+    title: "打断活跃请求数: " + le,
+    content: xc,
     icon: "xmark.circle",
     "icon-color": "#C5424A",
   };
-	let x = result.content;
-    $notification.post(result.title,'',x);
-    console.log("\n\n"+x+"\n");
+
+  if (noPanel) {
+    let x = result.content;
+    $notification.post(result.title, "", x);
+    le != 0 && console.log("\n\n" + x + "\n");
+  }
 })()
-  .catch((e) => {console.log(e)})
-  .finally(() => {$done(result)});
+  .catch((e) => {
+    console.log(e);
+  })
+  .finally(() => {
+    $done(result);
+  });
 
 function httpAPI(path = "", method = "POST", body = null) {
   return new Promise((resolve, reject) => {
     const tPr = new Promise((_, reject) => {
-      setTimeout(() => {reject("");resolve("");
+      setTimeout(() => {
+        reject("");
+        resolve("");
       }, 500);
     });
     const reqPr = new Promise((resolve) => {
       $httpAPI(method, path, body, resolve);
     });
-    Promise.race([reqPr, tPr]).then((result) => {
+    Promise.race([reqPr, tPr])
+      .then((result) => {
         resolve(result);
-      }).catch((error) => {
+      })
+      .catch((error) => {
         reject(error);
       });
   });
